b64982c8948cc73451bc123778b68eeae89b1175
Ádám Gólya <adam.stork@gmail.com>
2015-08-27 11:14:27 +0200

    refactor(rest): remove s as suffix

--- app/templates/rest.js
+++ app/templates/rest.js
@@ -1,22 +1,17 @@
 'use strict';
-var views = require('co-views');
 var parse = require('co-body');
-var <%= restName %>s = [];
-
-var render = views(__dirname + '/../views', {
-    map: { html: 'swig' }
-});
+var <%= restName %> = [];
 
 module.exports.<%= restName %> = function *<%= restName %>() {
-    this.body = yield render('<%= restName %>', { '<%= restName %>s': <%= restName %>s });
+    this.body = yield <%= restName %>;
 };
 
 module.exports.fetch_all = function *fetch_all() {
-    this.body = yield <%= restName %>s;
+    this.body = yield <%= restName %>;
 };
 
 module.exports.fetch = function *fetch(id) {
-    var <%= restName %> = <%= restName %>s[id];
+    var <%= restName %> = <%= restName %>[id] || null;
     if (!<%= restName %>) {
         this.throw(404, '<%= restName %> with id = ' + id + ' was not found');
     }
@@ -25,6 +20,6 @@ module.exports.fetch = function *fetch(id) {
 
 module.exports.create = function *create() {
     var <%= restName %> = yield parse(this);
-    var id = <%= restName %>s.push(<%= restName %>) - 1;
+    var id = <%= restName %>.push(<%= restName %>) - 1;
     <%= restName %>.id = id;
 };
