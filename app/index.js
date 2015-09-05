6d4bb3867d6cafe1f356473d76f1c3fc5d7913fc
Ádám Gólya <adam.stork@gmail.com>
2015-09-05 10:13:45 +0200

    refactor(index): change the order of appending the server.js

--- app/index.js
+++ app/index.js
@@ -15,14 +15,16 @@ var KoaRestGenerator = yeoman.generators.Base.extend({
             restName: this.restname,
             restCapitalName: this.restname[0].toUpperCase() + this.restname.slice(1)
         };
-        this.template("rest.js", "controllers/"+this.restname+".js", context);
-        this.template("model.js", "model/"+this.restname+".js", context);
-        this.template("spec.js", "test/"+this.restname+"Spec.js", context);
+
         var path = "server.js",
             file = this.readFileAsString(path);
 
         file += "var "+this.restname+" = require('./controllers/"+this.restname+"');\n";
         file += "router.get('/"+this.restname+"', auth.authed, "+this.restname+".fetch_all);\n";
+
+        this.template("rest.js", "controllers/"+this.restname+".js", context);
+        this.template("model.js", "model/"+this.restname+".js", context);
+        this.template("spec.js", "test/"+this.restname+"Spec.js", context);
 
         this.write(path, file);
     }
