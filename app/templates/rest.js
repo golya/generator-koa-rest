fc3551fb5c129f9f2a2d03760ac944e63ddb6ab6
Ádám Gólya <adam.stork@gmail.com>
2015-09-08 20:03:32 +0200

    fix(rest): add a better create method

--- app/templates/rest.js
+++ app/templates/rest.js
@@ -14,5 +14,7 @@ module.exports.fetch = function *fetch(id) {
 };
 
 module.exports.create = function *create() {
-    <%= restName %>.id = <%= restCapitalName %>.save();
+    new <%= restCapitalName %>({
+        name: test,
+    }).save();
 };
