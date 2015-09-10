bd257330985a3d4fdea8427c32e1e7e57ae60131
Ádám Gólya <adam.stork@gmail.com>
2015-09-10 22:10:00 +0200

    feat(index): add option to spec

--- app/index.js
+++ app/index.js
@@ -9,6 +9,7 @@ var KoaRestGenerator = yeoman.generators.Base.extend({
         yeoman.Base.apply(this, arguments);
         this.argument('restname', { type: String, required: true });
         this.argument('ignoremodel', { type: String, required: true, default: true });
+        this.argument('ingoretest', { type: String, required: true, default: true });
     },
 
     generateController: function(){
@@ -22,12 +23,15 @@ var KoaRestGenerator = yeoman.generators.Base.extend({
 
         file += "var "+this.restname+" = require('./controllers/"+this.restname+"');\n";
         file += "router.get('/"+this.restname+"', auth.authed, "+this.restname+".fetch_all);\n";
-
+        
         this.template("rest.js", "controllers/"+this.restname+".js", context);
         if (!this.ignoremodel) {
             this.template("model.js", "model/" + this.restname + ".js", context);
         }
-        this.template("spec.js", "test/"+this.restname+"Spec.js", context);
+
+        if (!this.ingoretest) {
+            this.template("spec.js", "test/" + this.restname + "Spec.js", context);
+        }
 
         this.write(path, file);
     }
