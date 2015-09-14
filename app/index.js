377c2df4af58b11f3cd5d8ed176b27f03d0ceb49
Ádám Gólya <adam.stork@gmail.com>
2015-09-14 09:44:58 +0200

    refactor(index): reorganize command line options

--- app/index.js
+++ app/index.js
@@ -8,9 +8,11 @@ var KoaRestGenerator = yeoman.generators.Base.extend({
     constructor: function () {
         yeoman.Base.apply(this, arguments);
         this.argument('restname', { type: String, required: true });
-        this.argument('ignoremodel', { type: String, required: true, default: true });
-        this.argument('ingoretest', { type: String, required: true, default: true });
-        this.argument('modeltype', { type: String, required: true, default: 'mongodb' });
+        this.option('admin', { alias: '-a', type: String, default: 'mongodb' });
+        this.option('modeltype', { type: String, defaults: 'mongodb' });
+        this.option('ignoremodel', { type: String, defaults: false });
+        this.option('ingoretest', { type: String, defaults: false });
+
     },
 
     generateController: function(){
@@ -19,15 +21,15 @@ var KoaRestGenerator = yeoman.generators.Base.extend({
             restCapitalName: this.restname[0].toUpperCase() + this.restname.slice(1)
         };
 
-        var path = "server.js",
+        var path = this.options.a ? "admin.js" : "server.js",
             file = this.readFileAsString(path);
 
         file += "var "+this.restname+" = require('./controllers/"+this.restname+"');\n";
         file += "router.get('/"+this.restname+"', auth.authed, "+this.restname+".fetch_all);\n";
 
         this.template("rest.js", "controllers/"+this.restname+".js", context);
-        if (!this.ignoremodel) {
-            switch(this.modeltype) {
+        if (!this.options.ignoremodel) {
+            switch(this.options.modeltype) {
                 case 'mongodb':
                     this.template("model.js", "model/" + this.restname + ".js", context);
                     break;
@@ -37,7 +39,7 @@ var KoaRestGenerator = yeoman.generators.Base.extend({
             }
         }
 
-        if (!this.ingoretest) {
+        if (!this.options.ingoretest) {
             this.template("spec.js", "test/" + this.restname + "Spec.js", context);
         }
 
