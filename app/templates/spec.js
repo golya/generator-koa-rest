d4d8b5bb688ed8a3632ef213d1ab1c01b2d4496f
Ádám Gólya <adam.stork@gmail.com>
2015-10-06 19:04:26 +0200

    feat(spec): add better auth logic in spec
    Signed-off-by: Ádám Gólya <adam.stork@gmail.com>

--- app/templates/spec.js
+++ app/templates/spec.js
@@ -7,7 +7,7 @@ var userAgent = superagent.agent(app.listen());
 var user = {email: 'brand@test.com', password: 'test'};
 
 describe('<%= restCapitalName %>', function () {
-    beforeEach(function (done) {
+    it('should auth with a user', function (done) {
         userAgent
             .post('/auth')
             .send(user)
@@ -16,16 +16,13 @@ describe('<%= restCapitalName %>', function () {
                 done();
             })
     });
-    describe('GET /<%= restName %>/', function () {
-        it('should return 200', function (done) {
-            userAgent
-                .get('/<%= restName %>')
-                .expect(200)
-                .end(function(err, result) {
-                    assert.equal(result.status, 200);
-                    done();
-                })
-        });
-
+    it('should return 200', function (done) {
+        userAgent
+            .get('/<%= restName %>')
+            .expect(200)
+            .end(function(err, result) {
+                assert.equal(result.status, 200);
+                done();
+            })
     });
 });
