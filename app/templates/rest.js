22721b057d8460b77dea59da61c34606bb541930
Ádám Gólya <adam.stork@gmail.com>
2015-10-13 21:06:54 +0200

    fix(rest): fix id source
    Signed-off-by: Ádám Gólya <adam.stork@gmail.com>

--- app/templates/rest.js
+++ app/templates/rest.js
@@ -6,8 +6,8 @@ module.exports.fetch_all = function *fetch_all() {
     this.body = yield <%= restCapitalName %>.find({});
 };
 
-module.exports.fetch = function *fetch(id) {
-    var fetched_<%= restName %> = yield <%= restCapitalName %>.findOne({_id: id}) || null;
+module.exports.fetch = function *fetch() {
+    var fetched_<%= restName %> = yield <%= restCapitalName %>.findOne({_id: this.params.id}) || null;
     if (!fetched_<%= restName %>) {
         this.throw(404, '<%= restCapitalName %> with id = ' + id + ' was not found');
     }
