6b0aff4f51472f57a492d66a7cd7a1e4c336125c
Ádám Gólya <adam.stork@gmail.com>
2015-10-25 16:47:44 +0100

    feat(model): add default updated_at field
    Signed-off-by: Ádám Gólya <adam.stork@gmail.com>

--- app/templates/model.js
+++ app/templates/model.js
@@ -5,7 +5,14 @@ var <%= restName %>Schema = new mongoose.Schema({
     'name': {
         type: String,
         required: true
+    },
+    updated_at: {
+        type: Date
     }
 });
 
+<%= restName %>Schema.pre('save', function(done) {
+    this.updated_at = new Date();
+    done();
+});
 module.exports = mongoose.model("<%= restCapitalName %>", <%= restName %>Schema);
